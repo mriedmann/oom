c8a6aa5746202701d92926781659aafb9766fc08
Stefan Maierhofer <sm@technikum-wien.at>
2015-12-29 18:02:07 +0100

    improved lessons/lesson1

--- lessons/lesson1/lesson1/Program.cs
+++ lessons/lesson1/lesson1/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// variables, primitive types (int, double, string), arrays, type inference
+// statements (if, for, while, do, foreach), operators
+// exception handling
+// collections (List<T>), generics
+
 namespace lesson1
 {
     class Program
